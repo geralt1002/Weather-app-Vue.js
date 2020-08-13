f26150291819fd747f3379604833f73cafb2f85e
Krzysiek <krzysztof.uminski1@gmail.com>
2020-08-13 20:03:02 +0200

    add build version

---
+++ precache-manifest.168e413e809617b260f3ce35fd099513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf2b800336d273afa98",
+    "url": "/Weather-app-vue.js/css/app.a9922294.css"
+  },
+  {
+    "revision": "37b121561096fd14ba0c",
+    "url": "/Weather-app-vue.js/css/chunk-vendors.689981cb.css"
+  },
+  {
+    "revision": "0d1d527b64fcf9f5c0f3bd1de447d05b",
+    "url": "/Weather-app-vue.js/index.html"
+  },
+  {
+    "revision": "5bf2b800336d273afa98",
+    "url": "/Weather-app-vue.js/js/app.4cbdcfc8.js"
+  },
+  {
+    "revision": "37b121561096fd14ba0c",
+    "url": "/Weather-app-vue.js/js/chunk-vendors.13cc6484.js"
+  },
+  {
+    "revision": "de634d89b7f8a557b5104a59f86c46fb",
+    "url": "/Weather-app-vue.js/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Weather-app-vue.js/robots.txt"
+  }
+]);
